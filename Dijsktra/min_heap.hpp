5df319615a0340c40e00d8b8010d0487631c1100
etromero <eromero@lexingtoninnovations.com>
2023-06-02 08:32:56 -0400

    Removed redudant lines

--- Dijsktra/min_heap.hpp
+++ Dijsktra/min_heap.hpp
@@ -17,25 +17,6 @@ class minHeapNode
     friend class Graph;
 };
 
-class minHeapNodeV2
-{
-    public:
-        minHeapNodeV2(int nodeId, int distance);
-        int getNodeId();
-        int getDist();
-
-        minHeapNodeV2* p_getNextNode();
-        void setNextNode(minHeapNodeV2* newNode);
-    
-    private:
-        int nodeId;
-        int distance;
-        minHeapNodeV2* p_nextNode;
-
-    friend class minHeapV2;
-    friend class Graph;
-};
-
 class minHeap
 {
     public:
@@ -65,35 +46,5 @@ class minHeap
 
         friend class Graph;
 };
-
-class minHeapV2
-{
-    public:
-        minHeapV2(const int capacity);
-        ~minHeapV2();
-
-        int parent(int i);
-        int Left(int i);
-        int Right(int i);
-
-        void Heapify(int i);
-        void heapInsert(int const nodeId, int const distance);
-        minHeapNodeV2* extractMin();
-        void decreaseDistance(int nodeId, int newDistance);
-
-        void moveUp(int i);
-        
-        int getSize();
-        int getNumofNodes();
-        void printHeap();
-
-    private:
-        int capacity;
-        minHeapNodeV2* p_list_head;
-        int numOfNodes;
-
-        friend class Graph;
-};
-
 
 #endif
